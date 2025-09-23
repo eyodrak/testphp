9b6006342196ec04045f67ca9ea231d9bf3aab4d
eyodrak <eyodrak@gmail.com>
2025-09-23 15:15:41 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,139 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="th" lang="th">
+
+<head>
+		<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
+
+<title>
+YodRak Karaoke
+</title>
+<style type="text/css">
+	
+table {
+    border-collapse: collapse;
+    border-spacing: 0;
+}
+
+div.styled-table {
+    display: inline-block;
+    width: 100%;
+}
+
+div.styled-table > table {
+    width:100%;
+    text-align: left;
+    white-space: normal;
+}
+
+div.styled-table > table > thead > tr > th {
+    padding: 10px 10px 9px 10px;
+    text-align: left;
+
+    margin-top: 10px;
+    background-color: #ECB825;
+    background-repeat: no-repeat;
+    background-image: -moz-linear-gradient(top, rgba(255, 255, 255, 0.2), rgba(0, 0, 0, 0.2));
+    background-image: -ms-linear-gradient(top, rgba(255, 255, 255, 0.2), rgba(0, 0, 0, 0.2));
+    background-image: -o-linear-gradient(top, rgba(255, 255, 255, 0.2), rgba(0, 0, 0, 0.2));
+    background-image: -webkit-linear-gradient(top, rgba(255, 255, 255, 0.2), rgba(0, 0, 0, 0.2));
+
+
+    text-shadow: rgba(0, 0, 0, 0.2) 0px -1px 0px;
+    color: white;
+}
+
+div.styled-table > table td {
+    padding: 10px 10px 7px 10px;
+}
+
+div.styled-table > table tbody {
+    margin-top: 3px;
+}
+
+div.styled-table > table tbody tr {
+    border: 1px solid #f0f0f0;
+    border-top: none;
+    background: transparent;
+}
+
+div.styled-table > table tfoot {
+    font-size: 0.9em;
+}
+
+  a:link {
+    text-decoration: none;
+	}
+
+a:visited {
+    text-decoration: none;
+	}
+
+a:hover {
+    text-decoration: none;
+	}
+
+a:active {
+    text-decoration: none;
+	}
+
+
+</style>
+</head>
+<body>
+<center>
+
+
+
+
+<center>
+<font face = "tahoma" size = "6">
+
+<div class="styled-table">
+<table class="custom-table">
+              <thead>
+                <tr>
+					 <th style="background-color:#ff8000; padding-top: 70px; padding-bottom:70px;" scope="col"><br><br><font size = +8><center>YodRak Karaoke Menu<center></font>
+					 <br><center><font size=+3>เลือกเมนูที่ต้องการด้านล่าง<br><br></font></center></th>
+				</tr>
+			  </thead>
+			  <thead>
+                <tr>
+					 <th style="background-color:pink; padding-top: 70px; padding-bottom:70px;" scope="col"><font size = +8><center><a href=queue.php><img src=queue.png width=50 height=50>    ดูคิวเพลง</a></center></font></th>
+				</tr>
+			  </thead>
+			  <thead>
+                <tr>
+					 <th style="background-color:yellow; padding-top: 70px; padding-bottom:70px;" scope="col"><font size = +8><center><a href=title.php?txtKeyword><img src=search.png width=50 height=50>    ค้นหาเพลง</a></center></font></th>
+				</tr>
+			  </thead>
+			   <thead>
+                <tr>
+					 <th style="background-color:red; padding-top: 70px; padding-bottom:70px;" scope="col"><font size = +8><center><a href=top50.php><img src=artists.png width=50 height=50>    เพลง TOP 50</a></center></font></th>
+				</tr>
+			  </thead>
+			   <thead>
+                <tr>
+					 <th style="background-color:gray; padding-top: 70px; padding-bottom:70px;" scope="col"><font size = +8><center><a href=newsong.php?Page=1><img src=newsong.png width=50 height=50>    เพลงใหม่</a></center></font></th>
+				</tr>
+			  </thead>
+			  <thead>
+                <tr>
+					 <th style="background-color:lightgreen; padding-top: 70px; padding-bottom:70px;" scope="col"><font size = +8><center><a href=allsongs.php?Page=1><img src=allsongs.png width=50 height=50>    รายชื่อเพลงทั้งหมด</a></center></font></th>
+				</tr>
+			  </thead>
+			   <thead>
+                <tr>
+					 <th style="background-color:lightblue; padding-top: 70px; padding-bottom:70px;" scope="col"><font size = +8><center><a href=artists.php?Page=1><img src=artists.png width=50 height=50>    รายชื่อศิลปินทั้งหมด</a></center></font></th>
+				</tr>
+			  </thead>
+			  <thead>
+				<tr>
+					 <td><br><br><font size = +8><center><img src="qr.jpg"></center></font></td>
+				</tr>
+			  </thead>
+</table>
+</div>
+</font>
+
+</center>
+</body>
+</html>
